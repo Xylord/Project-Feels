9e54be0c5f2a2b0f305a9f744eb377192a4ae424
ZL <zhenglong354@hotmail.com>
2016-07-28 23:52:26 -0400

    Added codes
    Added
    -Camera Control v.1.0.0
    -Camera Facing Billboard
    -Enemy.cs *Health Bar + function to debug*

---
+++ Code de ZL/CameraFacingBillboard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Attach this to a canvas
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    void Start()
+    {
+        //if ever you're too lazy to attach a camera manually... cuz ya know.
+        if (mainCamera == null)
+            mainCamera = GameObject.FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+            mainCamera.transform.rotation * Vector3.up);
+
+    }
+}
---
+++ Code de ZL/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//ATTACH THIS TO THE ENEMY
+//NOTE : The Enemy gameobject must contain the following : Canvas ("EnemyCanvas") -> Image("HealthBg") -> Image ("Health")
+public class Enemy : MonoBehaviour {
+
+    public int health, maxHealth;
+    private Image healthBar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        health = 100;
+        maxHealth = 100;
+        healthBar = transform.FindChild("EnemyCanvas").FindChild("HealthBg").FindChild("Health").GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //To debug
+        if (Input.GetKeyDown(KeyCode.J))
+            Hit(10);
+
+        if (health == 0)
+            Destroy(gameObject);          
+	
+	}
+
+    public void Hit(int damage)
+    {
+        health -= damage;
+        healthBar.fillAmount = (float)health / (float)maxHealth;
+    }
+}
