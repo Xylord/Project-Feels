15106892576b13567d6923bb489cfe31b01bb3a1
ZL <zhenglong354@hotmail.com>
2016-07-30 12:31:14 -0400

    Update v0.0.2
    Added :
    -Animation for Camera
    -Android/Iphone Touch imput to move camera.
    
    Note: CameraControl.cs is obsolete.

---
+++ Code de ZL/AnimateCamera.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//This script takes in charge to animate the camera to always move to the a selected object 
+//NOTE : the object must contain an empty gameObject of the name "FocusPoint".
+
+public class AnimateCamera : MonoBehaviour
+{
+    public float moveSpeed = 35.0f;
+    public GameObject targetObject;
+    private bool movingTowardsTarget = false;
+
+    private float lerpSpeed = 0.025f;
+    private Transform initialRot;
+    private Transform finalRot;
+    private bool inPosition;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+        //Activated by pressing E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (movingTowardsTarget == true)
+            {
+                movingTowardsTarget = false;
+            }
+            else
+            {
+                initialRot = gameObject.transform;
+                finalRot = targetObject.transform;
+                movingTowardsTarget = true;
+            }
+        }
+
+        if (movingTowardsTarget)
+        {
+            MoveTowardsTarget(targetObject);
+        }
+	}
+
+    public void MoveTowardsTarget(GameObject target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+
+        //Move the camera to the target position.
+        if (Vector3.Distance(transform.position,target.transform.position) < 0.1)
+        {
+            transform.position = target.transform.position;
+            inPosition = true;
+
+            //movingTowardsTarget = false;
+
+        }
+
+        //Rotate the camera towards the object.
+        if (inPosition)
+        {
+            if (Mathf.Abs(initialRot.localEulerAngles.y - finalRot.localEulerAngles.y) < 3)
+            {
+                //reset boolean variables
+                transform.rotation = target.transform.rotation;
+
+                movingTowardsTarget = false;
+                inPosition = false;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(initialRot.rotation, finalRot.rotation, Time.time * lerpSpeed);
+            }
+        }
+    }
+}
---
+++ Code de ZL/TouchCameraControl.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchCameraControl : MonoBehaviour
+{
+
+    public float moveSensitivityX = 1.0f;
+    public float moveSensitivtyY = 1.0f;
+
+    public bool updateZoomSensitivy = true;
+
+    public float orthographicZoomSpeed = 0.05f;
+
+    public float minZoom = 1.0f;
+    public float maxZoom = 20.0f;
+
+    public bool invertMoveX = false;
+    public bool invertMoveY = false;
+
+    private Camera _camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(updateZoomSensitivy)
+        {
+            moveSensitivityX = _camera.orthographicSize / 5.0f;
+            moveSensitivityX = _camera.orthographicSize / 5.0f;
+        }
+
+        Touch[] touches = Input.touches;
+
+        if (touches.Length > 0)
+        {
+            //Single touch 
+            if (touches.Length == 1)
+            {
+                if (touches[0].phase == TouchPhase.Moved)
+                {
+                    Vector2 delta = touches[0].deltaPosition;
+
+                    //Math for X
+                    float positionX = delta.x * moveSensitivityX * Time.deltaTime;       
+
+                    //Invertion adjustments    
+                    positionX = invertMoveX ? positionX : positionX * -1;
+
+                    //Math for Y
+                    float positionY = delta.y * moveSensitivtyY * Time.deltaTime;
+
+                    //Invertion adjustments    
+                    positionY = invertMoveY ? positionY : positionY * -1;
+
+                    //Update position of the camera
+                    _camera.transform.position += new Vector3(positionX, positionY, 0);
+
+                }
+            }
+
+            // Double  touch 
+            if (touches.Length == 2)
+            {
+                Touch toucheOne = touches[0];
+                Touch touchTwo = touches[1];
+
+                //to find the previous touch of in the previous frame
+                Vector2 touchOnePreviousPos = toucheOne.position - toucheOne.deltaPosition;
+                Vector2 touchTwoPreviousPos = touchTwo.position - touchTwo.deltaPosition;
+
+                float prevTouchDeltaMagnitude = (touchOnePreviousPos - touchTwoPreviousPos).magnitude;
+
+                float touchDeltaMagnitude = (toucheOne.position - touchTwo.position).magnitude;
+
+                float deltaMagDiff = prevTouchDeltaMagnitude - touchDeltaMagnitude;
+
+                _camera.orthographicSize += deltaMagDiff * orthographicZoomSpeed;
+                _camera.orthographicSize = Mathf.Clamp( _camera.orthographicSize, minZoom, maxZoom );
+
+            }
+        }
+	}
+}
